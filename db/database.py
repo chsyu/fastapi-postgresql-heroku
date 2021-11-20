0e426f3383ad25f93569cb354c35580373df3aaf
chsyu <chsyu@tea.ntue.edu.tw>
2021-11-20 14:21:12 +0800

    use dotenv to set DATABASEURL

--- db/database.py
+++ db/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgres://dgxnvlkpkrpuns:8f7a92153206032825c110c47a42c0b94a2c7b2fe2c65fb2f0afe52259587d24@ec2-54-145-9-12.compute-1.amazonaws.com:5432/df52nh7r70gr82"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
